a5a3c78cbae5e78f154a0ada50acaa9095fd03cf
Elad Sadeh <elad.sadeh@transperfect.com>
2024-02-05 14:34:10 -0500

    Remove logos

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      <h1>Vite + Amplify</h1>
       <div className="card">
         <button onClick={() => setShowAbout(!showAbout)}>About</button>
         {showAbout && <About />}
