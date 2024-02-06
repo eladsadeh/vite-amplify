8e8378776c23b108a307695c8ac86c1dc1df3f15
Elad Sadeh <elad.sadeh@transperfect.com>
2024-02-06 15:32:21 -0500

    Env vars demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import { About } from './About';
 import './App.css';
-// console.log('APP_VERSION', APP_VERSION);
-console.log(import.meta.env);
-// console.log('myvars.env', myvars.env);
+console.log('import.meta.env:', import.meta.env);
+console.log('process.env:', process.env);
 
 function App() {
   const [showAbout, setShowAbout] = useState(false);
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
       <h1>Vite + Amplify</h1>
       <h2>Test Amplify CLI</h2>
       <div className="card">
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// console.log('process:', process);
-// console.log(loadEnv('development', process.cwd(), ''));
-// console.log('process.env:', process.env);
-// console.log(MY_PW);
 
 // https://vitejs.dev/config/
 export default defineConfig({
