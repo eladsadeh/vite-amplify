db1943b3d16fda44231b5b78b51da43377754b17
Elad Sadeh <elad.sadeh@transperfect.com>
2024-02-06 15:38:02 -0500

    ignore ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { About } from './About';
 import './App.css';
 console.log('import.meta.env:', import.meta.env);
+// @ts-ignore
 console.log('process.env:', process.env);
 
 function App() {
